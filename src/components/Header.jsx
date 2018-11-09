8f3afd3bd2e3e5f653860de83827d19e8f893e1f
Ben Kiggen <benkiggen@gmail.com>
2018-11-09 10:59:34 -0800

    add header stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import barrels from '../assets/img/barrels.jpg';
+import barrels from '../assets/img/barrelicon.png';
 
 function Header(){
 
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const dateNow = new Date();
+  
   const headerStyles = {
     fontFamily: 'Montserrat',
     textAlign: 'center',
     width: '100%',
-    height: '80px',
+    height: '200px',
     marginTop: '0px'
   };
+  const imageStyles = {
+    height: '60px',
+    width: '60px',
+    marginTop: '50px'
+  }
+  const h1Styles = {
+    margin: '0px'
+  }
   
   return (
     <div style={headerStyles}>
-      <h1>Tap List for {dateNow.toLocaleDateString('en-Us', options)} </h1>
+      <img src={barrels} style={imageStyles}/>
+      <h1 style={h1Styles}>Mr. Cooper's</h1>
+      <h3 style={h1Styles}> Tap List: {dateNow.toLocaleDateString('en-Us', options)}</h3>
     </div>
   );
 }
