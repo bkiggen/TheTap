a974dc451755989be53f4643ef5108f4b8166e70
Ben Kiggen <benkiggen@gmail.com>
2018-11-09 12:01:33 -0800

    display beers from databse

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,15 +1,81 @@
 import React from 'react';
 import Beer from './Beer';
-// import PropTypes from 'prop-types';
 
+
+const  masterKegList = [
+  {
+    beerName: 'Ruby Zozzle',
+    brand: 'Hi-Wheel',
+    color: 'Sparkling Wine & Grapefruit',
+    abv: '6.8%',
+    price: '7',
+    status: '20'
+  },
+  {
+    beerName: 'Tart N Juicy',
+    brand: 'Epic',
+    color: 'Sour IPA',
+    abv: '4.5%',
+    price: '6',
+    status: '60'
+  },
+  {
+    beerName: 'Hamm\'s',
+    brand: 'Miller/Coors',
+    color: 'American Lager',
+    abv: '4.7%',
+    price: '3',
+    status: '65'
+  },
+  {
+    beerName: 'Prismatic',
+    brand: 'Ninkasi',
+    color: 'Juicy IPA',
+    abv:  '5.9%',
+    price: '6',
+    status: '75'
+  },
+  {
+    beerName: 'Juicy Haze',
+    brand: 'New Belgium',
+    color: 'India Pale Ale',
+    abv:  '7.5%',
+    price: '6',
+    status: '18'
+  },
+  {
+    beerName: '8 Hop',
+    brand: 'New Belgium',
+    color: 'Pale Ale',
+    abv:  '5.5%',
+    price: '6',
+    status: '58'
+  }
+];
+
+const beerListStyles = {
+  width: '80%',
+  border: '1px solid black',
+  height: '100vh',
+  margin: '0 auto',
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'center'
+};
 function BeerList() {
   return (
-    <div>
-      <h1>BeerList</h1>
-      <Beer></Beer>
+    <div style={beerListStyles}>
+      {masterKegList.map((keg, index) =>
+        <Beer beerName={keg.beerName}
+          brand={keg.brand}
+          color={keg.color}
+          abv={keg.abv}
+          price={keg.price}
+          status={keg.status}
+          key={index} />
+      )}
     </div>
   );
 }
-
 
 export default BeerList;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,31 +10,36 @@ function Header(){
     fontFamily: 'Montserrat',
     textAlign: 'center',
     width: '100%',
-    height: '200px',
+    height: '300px',
     marginTop: '0px'
-  }
+  };
 
   const imageStyles = {
     height: '60px',
     width: '60px',
     marginTop: '20px'
-  }
+  };
   
   const h1Styles = {
     margin: '5px'
-  }
+  };
   
   const hrStyles = {
-    width: '200px',
+    width: '300px',
     margin: '0 auto'
-  }
+  };
+  
+  const tapListStyles = {
+    fontStyle: 'italic',
+    margin: '5px'
+  };
   
   return (
     <div style={headerStyles}>
       <img src={barrels} style={imageStyles}/>
       <h1 style={h1Styles}>Mr. Cooper's</h1>
       <hr style={hrStyles}/>
-      <h1 style={h1Styles}>Tap List</h1>
+      <h1 style={tapListStyles}>Tap List</h1>
       <hr style={hrStyles}/>
       <h3 style={h1Styles}>  {dateNow.toLocaleDateString('en-Us', options)}</h3>
     </div>
